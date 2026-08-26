fa711a4b2c0bcb8e4afd4b0db138a5ad165c28fc
Chandrakant Singh <cks3383@gmail.com>
2026-08-26 14:11:34 +0530

    Update ports (1223 packages) — 2026-08-26 08:41 UTC

--- ports/xcb-utilities/build.sh
+++ ports/xcb-utilities/build.sh
@@ -39,27 +39,16 @@ meson() {
     command meson "$@"
   fi
 }
-rm -rf "$ALPS_WORK/$ALPS_NAME"
-mkdir -p "$ALPS_WORK/$ALPS_NAME"
-# BLFS ../file convention: stage patches/extra downloads beside the extracted tree
-for _f in ${ALPS_PATCH_FILES:-}; do
-  [[ -n "$_f" && -e "$ALPS_SOURCES/$_f" ]] || continue
-  ln -f "$ALPS_SOURCES/$_f" "$ALPS_WORK/$ALPS_NAME/$_f" 2>/dev/null \
-    || cp -a "$ALPS_SOURCES/$_f" "$ALPS_WORK/$ALPS_NAME/$_f"
-done
-tar -xf "$ALPS_SOURCES/$ALPS_TARBALL" -C "$ALPS_WORK/$ALPS_NAME"
-mapfile -t _tops < <(find "$ALPS_WORK/$ALPS_NAME" -mindepth 1 -maxdepth 1 -type d | sort)
-if [[ ${#_tops[@]} -ne 1 ]]; then
-  echo "error: expected one source dir in $ALPS_WORK/$ALPS_NAME" >&2
-  exit 1
-fi
-cd "${_tops[0]}"
+# xorg_batch — multi-tarball list (no single ALPS_TARBALL unpack)
 # BLFS Xorg build environment (recommended /usr prefix)
 : "${XORG_PREFIX:=/usr}"
 : "${XORG_CONFIG:=--prefix=$XORG_PREFIX --sysconfdir=/etc --localstatedir=/var --disable-static}"
 export XORG_PREFIX XORG_CONFIG
 
 # --- commands from BLFS ---
+rm -rf "$ALPS_WORK/$ALPS_NAME"
+mkdir -p "$ALPS_WORK/$ALPS_NAME"
+cd "$ALPS_WORK/$ALPS_NAME"
 cat > xcb-utils.md5 << "EOF"
 a67bfac2eff696170259ef1f5ce1b611  xcb-util-image-0.4.1.tar.xz
 fbdc05f86f72f287ed71b162f1a9725a  xcb-util-keysyms-0.4.1.tar.xz
@@ -68,20 +57,20 @@ fbdc05f86f72f287ed71b162f1a9725a  xcb-util-keysyms-0.4.1.tar.xz
 e85bccd1993992be07232f8b80a814c8  xcb-util-cursor-0.1.6.tar.xz
 322cf9df283625223933ffcecb4a1cca  xcb-util-errors-1.0.1.tar.xz
 EOF
-mkdir xcb-utils
-cd    xcb-utils
-grep -v '^#' ../xcb-utils.md5 | awk '{print $2}' | wget -i- -c \
-     -B https://xorg.freedesktop.org/archive/individual/lib/
-md5sum -c ../xcb-utils.md5
-as_root()
-{
-  if   [ $EUID = 0 ];        then $*
-  elif [ -x /usr/bin/sudo ]; then sudo $*
-  else                            su -c \\"$*\\"
+mkdir -p xcb-utils
+cd xcb-utils
+BASE_URL="https://xorg.freedesktop.org/archive/individual/lib/"
+while read -r sum file; do
+  [[ -z "${file:-}" || "$sum" =~ ^# ]] && continue
+  if [[ ! -f "$file" ]]; then
+    if [[ -f "$ALPS_SOURCES/$file" ]]; then
+      ln -f "$ALPS_SOURCES/$file" "$file" 2>/dev/null || cp -a "$ALPS_SOURCES/$file" "$file"
+    else
+      wget -c -O "$file" "${BASE_URL}${file}"
+    fi
   fi
-}
-export -f as_root
-bash -e
+done < <(grep -v '^#' ../xcb-utils.md5)
+md5sum -c ../xcb-utils.md5
 for package in $(grep -v '^#' ../xcb-utils.md5 | awk '{print $2}')
 do
   packagedir=${package%.tar.?z*}
@@ -89,8 +78,7 @@ do
   pushd $packagedir
      ./configure $XORG_CONFIG
      make
-     as_root make install
+     make install
   popd
   rm -rf $packagedir
 done
-exit
